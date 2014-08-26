398012cb32281adc6349e14d521975e75e881c5f
Fernando <fernando@torcelly.com>
2014-08-26 01:23:20 +0100

    Update app.js
    Update file for new episode streams feature (version 1.3.2+)

--- template.stm/app.js
+++ template.stm/app.js
@@ -32,7 +32,57 @@ if (argv.m) {
 	
 	
 	
-	// MODE 2 : other languages. Extenal API code (recommended)
+	// MODE 2 : other languages. External API code (recommended)
+	// --------------------------------------------------------
+	
+	var url = 'ENTER YOUR HTTP HERE';
+	
+	request(url, function(error, response, html) {
+	
+		if(!error) {
+			console.log(JSON.stringify(html));
+		}
+		else {
+			var empty = [];
+			console.log(JSON.stringify(empty));
+		}
+	});
+	
+	// --------------------------------------------------------
+}
+
+
+// EPISODES
+if (argv.e) {	
+	/*
+	
+	id : show TVDB code (required)
+	link : url or magnet to get episode. Support: youtube, magnet link, torrent, direct URL to video (required)
+	season : number of season
+	episode : number of episode
+	
+	quality : link quality: 1080p, 720p o 480p (optional)
+	seeders : number of seeders if link is a magnet or torrent file (optional)
+	size : size del link (optional)
+	
+	*/
+	
+	// Select one option and comment the code not used:
+	
+	
+	// MODE 1 : node.js code
+	// ---------------------
+	
+	var episodes = [];
+	
+	// Here must be a console.log() to return JSON.
+	console.log(JSON.stringify(episodes));
+	
+	// ----------------------
+	
+	
+	
+	// MODE 2 : other languages. External API code (recommended)
 	// --------------------------------------------------------
 	
 	var url = 'ENTER YOUR HTTP HERE';
